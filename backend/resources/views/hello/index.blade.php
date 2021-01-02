aa32f9c4e79d27dbd03e14b97fdbb8abd1a01edf
disk042 <rey.disk042@gmail.com>
2021-01-02 18:43:56 +0900

    p94まで完了

--- backend/resources/views/hello/index.blade.php
+++ backend/resources/views/hello/index.blade.php
@@ -1,26 +1,27 @@
-<html>
-<head>
-    <title>Hello/Index</title>
-    <style>
-    body {font-size: 16px; color: #999; }
-    h1 { font-size:50pt; text-align: right; color: #f6f6f6;
-        margin: -20px 0px -30px 0px; letter-spacing: -4pt;}
-    </style>
-</head>
-<body>
-    <h1>Blade/Index</h1>
-    <p>&#064;whileディレクティブの例</p>
-    <ol>
-    @php
-    $counter = 0;
-    @endphp
-    @while ($counter < count($data))
-    <li>{{ $data[$counter] }}</li>
-    @php
-    $counter++;
-    @endphp
-    @endwhile
-    </ol>
+@extends('layouts.helloapp')
 
-</body>
-</html>
+@section('title', 'Index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツです。</p>
+    <p>必要なだけ記述ができます。</p>
+
+    @component('components.message')
+    @slot('msg_title')
+    CAUTION!
+    @endslot
+
+    @slot('msg_content')
+    これはメッセージの表示です。
+    @endslot
+    @endcomponent
+@endsection
+
+@section('footer')
+copyright 2021 tashiro.
+@endsection
