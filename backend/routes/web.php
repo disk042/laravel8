2b2394c093587195f8f45f66377b4e3976946752
disk042 <rey.disk042@gmail.com>
2021-02-20 00:06:47 +0900

    Chapter7-2

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -54,3 +54,6 @@ Route::get('board/add', [BoardController::class, 'add']);
 Route::post('board/add', [BoardController::class, 'create']);
 Route::resource('rest', RestappController::class);
 Route::get('hello/rest', [HelloController::class, 'rest']);
+
+Route::get('hello/session', [HelloController::class, 'ses_get']);
+Route::post('hello/session', [HelloController::class, 'ses_put']);
