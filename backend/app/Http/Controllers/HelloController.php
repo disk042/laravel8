72bb85223c7fccdadb832a87d8391cf38332dfa2
disk042 <rey.disk042@gmail.com>
2021-02-20 22:06:31 +0900

    Chapter7-3

--- backend/app/Http/Controllers/HelloController.php
+++ backend/app/Http/Controllers/HelloController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Person;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
@@ -12,8 +13,10 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::table('people')->orderBy('age', 'desc')->get();
-        return view('hello.index', ['items' => $items]);
+        $sort = $request->sort;
+        $items = Person::orderBy($sort, 'asc')->simplePaginate(5);
+        $param = ['items' => $items, 'sort' => $sort];
+        return view('hello.index', $param);
     }
 
     public function post(Request $request)
